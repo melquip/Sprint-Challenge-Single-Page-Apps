96c75924be99df7f89bafb04f47ddffbe37c9a5f
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-20 08:12:43 +0100

    Fetched List of characters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
+import axios from 'axios';
 
 
 export default function App() {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+	const [characters, setCharacters] = useState([])
+	useEffect(() => {
+		axios.get('https://rick-api.herokuapp.com/api/')
+		.then(res => {
+			setCharacters(res.characters);
+		})
+		.catch(error => console.error(error))
+		return () => {
+			console.log('cleanup')
+		};
+	}, [])
+	return (
+		<main>
+			<Header />
+		</main>
+	);
 }
