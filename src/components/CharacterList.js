cfdc96df97a352b33bb6879a45c76b0d8933b743
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-20 08:22:10 +0100

    Displayed the characters state

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+	const [characters, setCharacters] = useState([]);
+	useEffect(() => {
+		// TODO: Add API Request here - must run in `useEffect`
+		axios.get('https://rick-api.herokuapp.com/api/')
+			.then(res => {
+				setCharacters(res.characters);
+			})
+			.catch(error => console.error(error))
+		return () => {
+			console.log('cleanup')
+		};
+		//  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+	}, []);
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
-
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+	return (
+		<section className="character-list">
+			<h2>TODO: `array.map()` over your state here!</h2>
+			{
+				characters.map(char => <CharacterCard data={char} />)
+			}
+		</section>
+	);
 }
