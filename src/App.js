fd41a77f4c98d6c826634ba333838548f3a0737a
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-20 08:21:34 +0100

    Added some routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
-import axios from 'axios';
+import CharacterList from "./components/CharacterList.js";
+import WelcomePage from "./components/WelcomePage.js";
 
 
 export default function App() {
-	const [characters, setCharacters] = useState([])
-	useEffect(() => {
-		axios.get('https://rick-api.herokuapp.com/api/')
-		.then(res => {
-			setCharacters(res.characters);
-		})
-		.catch(error => console.error(error))
-		return () => {
-			console.log('cleanup')
-		};
-	}, [])
 	return (
 		<main>
 			<Header />
+			<Route exact path="/" component={WelcomePage} />
+			<Route path="/characters" component={CharacterList} />
 		</main>
 	);
 }
